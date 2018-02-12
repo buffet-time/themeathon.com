1d4746a14762a4dde6f35e228206751504e70480
Buffet Time <radd86@gmail.com>
2018-02-11 20:30:27 -0500

    removed dialog and changed submit button hyperlink

--- src/app/material-import.module.ts
+++ src/app/material-import.module.ts
@@ -3,7 +3,6 @@ import {
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
-    MatDialogModule,
     MatCardModule
 } from '@angular/material';
 
@@ -12,14 +11,12 @@ import {
         MatToolbarModule,
         MatButtonModule,
         MatSidenavModule,
-        MatDialogModule,
         MatCardModule
     ],
     exports: [
         MatToolbarModule,
         MatButtonModule,
         MatSidenavModule,
-        MatDialogModule,
         MatCardModule
     ]
 })
