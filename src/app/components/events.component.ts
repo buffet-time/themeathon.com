82704c2fd7191be55d85069c60d04de3363675b6
Buffet Time <radd86@gmail.com>
2018-05-30 13:51:30 -0400

    Minor Changes

--- src/app/components/events.component.ts
+++ src/app/components/events.component.ts
@@ -7,23 +7,5 @@ import { Component } from '@angular/core';
 })
 export class EventsComponent 
 {
-    navigateToDiscord() 
-    {
-      window.open("https://discordapp.com/invite/wd5GwXK");
-    }
     
-    navigateToTwitch() 
-    {
-      window.open("https://www.twitch.tv/Themeathon");
-    }
-    
-    navigateToTwitter() 
-    {
-      window.open("https://twitter.com/Themeathon");
-    }
-
-    navigateToYoutube() 
-    {
-      window.open("https://www.youtube.com/channel/UCoeWJ8x6jM_tN5AnBqLzQfg");
-    }
 }
